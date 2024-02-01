84505d3da79dd15b9c3b90ef58f4456c2766a8be
koki-algebra <yotsuba00040@gmail.com>
2024-02-01 20:26:18 +0900

    add prettier

--- src/util/investment.ts
+++ src/util/investment.ts
@@ -2,39 +2,41 @@
  * Investment parameters interface.
  */
 interface InvestmentParams {
-	initialInvestment: number; // The initial investment amount
-	annualInvestment: number; // The amount invested every year
-	expectedReturn: number; // The expected (annual) rate of return
-	duration: number; // The investment duration (time frame) in years
+  initialInvestment: number; // The initial investment amount
+  annualInvestment: number; // The amount invested every year
+  expectedReturn: number; // The expected (annual) rate of return
+  duration: number; // The investment duration (time frame) in years
 }
-
 
 /**
  * Calculate investment results based on provided parameters.
  * @param {InvestmentParams} investmentParams - Investment parameters.
  * @returns {Array<Object>} - Array containing investment data for each year.
  */
-export function calculateInvestmentResults(investmentParams: InvestmentParams): Array<{
-	year: number; // year identifier
-	interest: number; // the amount of interest earned in this year
-	valueEndOfYear: number; // investment value at end of year
-	annualInvestment: number; // investment added in this year
+export function calculateInvestmentResults(
+  investmentParams: InvestmentParams
+): Array<{
+  year: number; // year identifier
+  interest: number; // the amount of interest earned in this year
+  valueEndOfYear: number; // investment value at end of year
+  annualInvestment: number; // investment added in this year
 }> {
-	const annualData = [];
-	let investmentValue = investmentParams.initialInvestment;
+  const annualData = [];
+  let investmentValue = investmentParams.initialInvestment;
 
-	for (let i = 0; i < investmentParams.duration; i++) {
-		const interestEarnedInYear = investmentValue * (investmentParams.expectedReturn / 100);
-		investmentValue += interestEarnedInYear + investmentParams.annualInvestment;
-		annualData.push({
-			year: i + 1, // year identifier
-			interest: interestEarnedInYear, // the amount of interest earned in this year
-			valueEndOfYear: investmentValue, // investment value at end of year
-			annualInvestment: investmentParams.annualInvestment, // investment added in this year
-		});
-	}
+  for (let i = 0; i < investmentParams.duration; i++) {
+    const interestEarnedInYear =
+      investmentValue * (investmentParams.expectedReturn / 100);
+    investmentValue += interestEarnedInYear + investmentParams.annualInvestment;
+    annualData.push({
+      year: i + 1, // year identifier
+      interest: interestEarnedInYear, // the amount of interest earned in this year
+      valueEndOfYear: investmentValue, // investment value at end of year
+      annualInvestment: investmentParams.annualInvestment, // investment added in this year
+    });
+  }
 
-	return annualData;
+  return annualData;
 }
 
 /**
@@ -42,9 +44,9 @@ export function calculateInvestmentResults(investmentParams: InvestmentParams): 
  * This object offers a "format()" method that can be used to format numbers as currency.
  * Example Usage: formatter.format(1000) => yields "$1,000"
  */
-export const formatter = new Intl.NumberFormat('en-US', {
-	style: 'currency',
-	currency: 'USD',
-	minimumFractionDigits: 0,
-	maximumFractionDigits: 0,
+export const formatter = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+  minimumFractionDigits: 0,
+  maximumFractionDigits: 0,
 });
