fcb47807b10c51c648e8c04d61e9b7d7815934b9
koki-algebra <yotsuba00040@gmail.com>
2024-02-03 14:20:15 +0900

    fix import order

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import "./index.css";
-import Header from "./components/Header";
-import UserInput from "./components/UserInput";
-import Results from "./components/Results";
-import { InvestmentParams } from "./types/investment";
 import { useState } from "react";
+
+import Header from "./components/Header";
+import Results from "./components/Results";
+import UserInput from "./components/UserInput";
+import "./index.css";
+import { InvestmentParams } from "./types/investment";
 
 function App() {
   const [userInput, setUserInput] = useState<InvestmentParams>({
